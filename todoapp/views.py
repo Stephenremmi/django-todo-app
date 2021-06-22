b6518d18614e01e5554faefa06489960489ee1e2
stephen remmi <stephenremmi@gmail.com>
2021-06-22 11:16:03 +0300

    Make migratons and migrate

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from 
 # Create your views here.
+# Create List View to display List of Todo items
+class TaskList(ListView):
+    model = Task
 
-def home(request):
-    return HttpResponse("Hello World!!")
+
+    
