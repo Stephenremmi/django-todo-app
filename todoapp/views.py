f70a6ef206f8d9d91760a4afe3dfa74409d8519a
stephen remmi <stephenremmi@gmail.com>
2021-06-22 13:14:51 +0300

    Add CRUD functionalites to the app

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,10 +1,34 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from 
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
+from .models import Task
 # Create your views here.
 # Create List View to display List of Todo items
 class TaskList(ListView):
     model = Task
+    context_object_name = 'tasks'
 
+class TaskDetail(DetailView):
+    model = Task
+    context_object_name = 'task'
+    template_name = 'todoapp/task.html'
+
+class TaskCreate(CreateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class TaskUpdate(UpdateView):
+    model = Task
+    fields = '__all__'
+    success_url = reverse_lazy('tasks')
+
+class DeleteView(DeleteView):
+    model = Task
+    context_object_name = 'task'
+    success_url = reverse_lazy('tasks')
 
     
